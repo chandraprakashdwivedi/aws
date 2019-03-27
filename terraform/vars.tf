e24e0097873c0bcbfdedf7b9edf2fe18c9c4279e
CP <chandra.prakash363@gmail.com>
2019-03-27 23:00:45 +0530

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "myRegion"{
+	description = "Name of the current Stack"
+}
+
+variable "myBucketName" {
+	description = "The AWS access key."
+}
+
+variable "myBucketTag" {
+	description = "The AWS access key."
+}
